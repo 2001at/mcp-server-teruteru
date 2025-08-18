0ecfe9566635830d5f48ef0649558d4dd748d261
2001at <atsu.iida0415@gmail.com>
2025-08-18 19:26:13 +0900

    feat: implement playScale function for serial communication with buzzer

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 
-import { z } from "zod";
 import dotenv from "dotenv";
 import { SerialService } from "./serial_service.js";
+import { playScale } from "./buzzer.js";
 
 dotenv.config();
 
@@ -15,46 +15,7 @@ const server = new McpServer({
   version: "1.0.0",
 });
 
-// @ts-ignore
-server.tool(
-  "playScale",
-  "SP32 にシリアルで「周波数,鳴らす長さ」を送信すると、圧電ブザーでその通りに音を鳴らします。{frequency} は 20Hz から 20kHz の範囲で指定できます。また、{duration} は 100ms から 5000msの範囲で指定できます。",
-  {
-    reason: z.string().describe("ブザーを再生する理由").optional(),
-    frequency: z
-      .number()
-      .int()
-      .min(20)
-      .max(20000)
-      .describe("鳴らす周波数（Hz）"),
-    duration: z
-      .number()
-      .int()
-      .min(100)
-      .max(5000)
-      .describe("鳴らす長さ（ミリ秒）"),
-  },
-  async ({
-    reason,
-    frequency,
-    duration,
-  }: {
-    reason?: string;
-    frequency: number;
-    duration: number;
-  }) => {
-    const command = `${frequency},${duration}`;
-    await serial.write(command);
-    return {
-      content: [
-        {
-          type: "text",
-          text: `Successfully played sound at ${frequency} Hz for ${duration} ms.`,
-        },
-      ],
-    };
-  }
-);
+playScale(server, serial);
 
 async function main(): Promise<void> {
   console.log("Starting MCP Server with stdio transport...");
---
+++ src/buzzer.ts
@@ -0,0 +1,49 @@
+import type { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import type { SerialService } from "./serial_service.js";
+import z from "zod";
+
+export const playScale = async (
+  mcpServer: McpServer,
+  serialService: SerialService
+) => {
+  // @ts-ignore
+  mcpServer.tool(
+    "playScale",
+    "SP32 にシリアルで「周波数,鳴らす長さ」を送信すると、圧電ブザーでその通りに音を鳴らします。{frequency} は 20Hz から 20kHz の範囲で指定できます。また、{duration} は 100ms から 5000msの範囲で指定できます。",
+    {
+      reason: z.string().describe("ブザーを再生する理由").optional(),
+      frequency: z
+        .number()
+        .int()
+        .min(20)
+        .max(20000)
+        .describe("鳴らす周波数（Hz）"),
+      duration: z
+        .number()
+        .int()
+        .min(100)
+        .max(5000)
+        .describe("鳴らす長さ（ミリ秒）"),
+    },
+    async ({
+      reason,
+      frequency,
+      duration,
+    }: {
+      reason?: string;
+      frequency: number;
+      duration: number;
+    }) => {
+      const command = `${frequency},${duration}`;
+      await serialService.write(command);
+      return {
+        content: [
+          {
+            type: "text",
+            text: `Successfully played sound at ${frequency} Hz for ${duration} ms.`,
+          },
+        ],
+      };
+    }
+  );
+};
